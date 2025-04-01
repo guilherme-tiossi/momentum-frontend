92f5e75999269aea41573dd2b696bb094006600d
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2025-04-01 08:57:58 -0300

    feat(login): adjusted login page to be more similar to wireframe

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,13 +6,15 @@
 
 <style scoped>
 .card {
-  background: #EDEDED;
+  display: grid;
+  grid-template-columns: 1fr 2px 1fr;
+  background: #e6e6e6;
   padding: 2rem;
   border-radius: 10px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
-  width: 90%;
-  max-width: 1000px;
-  height: 70%;
+  width: 160vh;
+  height: 60vh;
   text-align: center;
+  padding: 10vh;
 }
 </style>
