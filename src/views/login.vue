261edcaecb07bd39f12142260fe8c74dd84a0ec8
Guilherme Tiossi <guilherme.tiossi@ditcom.com.br>
2025-04-01 08:57:58 -0300

    feat(login): adjusted login page to be more similar to wireframe

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,33 +1,106 @@
 <template>
-    <div class="container">
-      <Card>
-        <h2>Login</h2>
-        <input type="email" placeholder="Email" v-model="email" />
-        <input type="password" placeholder="Senha" v-model="password" />
-        <button @click="login">Entrar</button>
-      </Card>
+  <div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="card p-5 shadow-lg custom-card" style="max-width: 900px; width: 100%">
+      <div class="row g-0">
+        <div class="col-md-6 d-flex flex-column align-items-center text-center p-4">
+          <div class="custom-img rounded"></div>
+          <p class="mt-3 custom-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+        
+        <div class="col-md-1 d-flex align-items-center">
+          <div class="custom-divider"></div>
+        </div>
+        
+        <div class="col-md-5 d-flex flex-column justify-content-center p-4">
+          <input type="email" class="form-control mb-3 custom-input" placeholder="E-mail" v-model="email" />
+          <input type="password" class="form-control mb-3 custom-input" placeholder="Password" v-model="password" />
+          <button class="btn w-100 mb-2 custom-button" @click="login">Login</button>
+          <div class="custom-hr mb-3"></div>
+          <button class="btn btn-link custom-button-clear mb-2">Forgot password?</button>
+          <button class="btn w-100 custom-button">Create new account</button>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from "vue";
-  import Card from "../components/Card.vue";
-  
-  const email = ref("");
-  const password = ref("");
-  
-  const login = () => {
-    console.log("Logando com:", email.value, password.value);
-  };
-  </script>
-  
-  <style scoped>
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    /* padding: 2rem; */
-  }
-  </style>
-  
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+
+const login = () => {
+  console.log("Logando com:", email.value, password.value);
+};
+</script>
+
+<style scoped>
+.custom-card {
+  background-color: #e6e6e6;
+  border: none;
+  border-radius: 10px;
+}
+
+.custom-img {
+  width: 150px;
+  height: 150px;
+  background: #d0d0d0;
+}
+
+.custom-paragraph {
+  font-family: 'Inter', sans-serif;
+  font-size: 1.1rem;
+  text-align: center;
+}
+
+.custom-divider {
+  width: 1.3px;
+  background: #676767;
+  height: 100%;
+}
+
+.custom-hr {
+  height: 1.3px;
+  background: #676767;
+  width: 100%;
+  opacity: 1; /* Sobrescreve o estilo do Bootstrap */
+}
+
+.custom-input {
+  background-color: #d0d0d0 !important;
+  border: none !important;
+  border-radius: 10px !important;
+  height: 50px;
+  font-family: 'Inter', sans-serif;
+}
+
+.custom-input::placeholder {
+  color: #676767;
+}
+
+.custom-button {
+  background-color: #d0d0d0 !important;
+  border: none !important;
+  border-radius: 10px !important;
+  height: 50px;
+  font-family: 'Inter', sans-serif;
+  font-weight: bold;
+  color: black;
+}
+
+.custom-button-clear {
+  background-color: transparent !important;
+  color: #676767 !important;
+  text-decoration: none !important;
+  font-family: 'Inter', sans-serif;
+  font-weight: bold;
+  padding: 0;
+}
+
+/* Sobrescreve o hover do Bootstrap */
+.custom-button-clear:hover {
+  color: #676767 !important;
+  text-decoration: underline !important;
+}
+</style>
