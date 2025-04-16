1014c59a7c688f07efe89a82b2dff1b9a2dbe9c8
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2025-04-16 19:50:45 -0300

    feat(create button): added legends to each small button

--- src/components/CreateButton.vue
+++ src/components/CreateButton.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="fab-container" v-click-outside="closeIfOpen">
-    <transition-group name="fade-up" tag="div">
-      <button
-        v-if="isOpen"
-        key="btn1"
-        class="button small-button"
-        style="bottom: 110px; background-color: #d9d9d9"
-      >
-        >:)
-      </button>
-      <button
-        v-if="isOpen"
-        key="btn2"
-        class="button small-button"
-        style="bottom: 180px; background-color: #d9d9d9"
-      >
-        :o
-      </button>
+    <transition-group name="fade-up" tag="div" class="fab-items">
+      <div v-if="isOpen" class="fab-item" key="item1">
+        <div class="fab-inner">
+          <div class="label">New Task</div>
+          <button class="button small-button">>:)</button>
+        </div>
+      </div>
+      <div v-if="isOpen" class="fab-item" key="item2">
+        <div class="fab-inner">
+          <div class="label">New Recurrent Task</div>
+          <button class="button small-button">:o</button>
+        </div>
+      </div>
+      <div v-if="isOpen" class="fab-item" key="item3">
+        <div class="fab-inner">
+          <div class="label">New Post</div>
+          <button class="button small-button">:3</button>
+        </div>
+      </div>
     </transition-group>
 
     <button class="main-button button" @click="toggle">
@@ -30,6 +32,7 @@
 
 <script setup>
 import { ref } from "vue";
+
 const isOpen = ref(false);
 const toggle = () => (isOpen.value = !isOpen.value);
 
@@ -62,35 +65,98 @@ const vClickOutside = {
   bottom: 35px;
 }
 
+.fab-items {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  position: absolute;
+  bottom: 64px;
+  right: 5px;
+}
+
+.fab-item {
+  display: flex;
+  align-items: center;
+}
+
+.fab-inner {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.label {
+  font-family: "Inter", sans-serif;
+  background-color: rgba(0, 0, 0, 1);
+  color: white;
+  padding: 10px 15px;
+  margin-right: 10px;
+  border-radius: 15px;
+  white-space: nowrap;
+  max-width: 200px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin-bottom: 10px;
+  display: inline-block;
+  position: relative;
+  opacity: 0;
+  transition: background-color 0.2s ease, opacity 0.2s ease;
+}
+
+.label::after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  right: -10px;
+  transform: translateY(-50%);
+  width: 0;
+  height: 0;
+  border-top: 10px solid transparent;
+  border-bottom: 10px solid transparent;
+  border-left: 10px solid rgba(0, 0, 0, 1);
+  opacity: 1;
+  pointer-events: none;
+  transition: border-left-color 0.2s ease;
+}
+
+.fab-item:hover .label::after {
+  border-left-color: rgba(0, 0, 0, 0.5);
+}
+
+.fab-item:hover .label {
+  overflow: visible;
+  opacity: 1;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
 .button {
   width: 65px;
   height: 65px;
-  background: #b9b9b9;
   border: 0px;
   border-radius: 45%;
   font-size: 50px;
   color: white;
   z-index: 1;
-
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
   display: flex;
   justify-content: center;
   align-items: center;
-  transition: background 0.3s;
+  transition: 0.3s;
 }
 
 .button:hover {
-  background: radial-gradient(
-    ellipse 70% 75% at center,
-    #c1c1c1,
-    #9c9c9c
-  ) !important;
-  color: #e6e6e6;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.35);
 }
 
 .main-button {
-  background: radial-gradient(ellipse 70% 75% at center, #cdcdcd, #a7a7a7);
+  background-color: #c0c0c0;
   z-index: 2;
   position: relative;
+}
+
+.main-button:hover {
+  background-color: #aeaeae;
+  color: #e6e6e6;
 }
 
 .plus-icon {
@@ -129,7 +195,7 @@ const vClickOutside = {
 }
 
 .small-button {
-  background: radial-gradient(ellipse 70% 75% at center, #cfcfcf, #b1b1b1);
+  background-color: #b7b7b7 !important;
   width: 55px;
   height: 55px;
   font-size: 26px;
@@ -141,11 +207,7 @@ const vClickOutside = {
 
 .small-button:hover {
   color: #fff;
-  background: radial-gradient(
-    ellipse 70% 75% at center,
-    #bfbfbf,
-    #9e9e9e
-  ) !important;
+  background-color: #a8a8a8 !important;
 }
 
 .fade-up-enter-active,
